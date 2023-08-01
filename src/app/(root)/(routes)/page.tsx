a7ae0948345a37241ab2381640bb386233bd1fbc
xuniorss <fortunatojunior2020@gmail.com>
2023-08-01 18:16:21 -0300

    feat:Companion List

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,14 +1,26 @@
 import { Categories } from '@/components/Categories'
+import { Companions } from '@/components/Companions'
 import { SearchInput } from '@/components/SearchInput'
 import prismadb from '@/lib/prismadb'
 
-export default async function RootPage() {
+interface RootPageProps {
+	searchParams: { categoryId: string; name: string }
+}
+
+export default async function RootPage({ searchParams }: RootPageProps) {
+	const data = await prismadb.companion.findMany({
+		where: { categoryId: searchParams.categoryId, name: searchParams.name },
+		orderBy: { categoryId: 'desc' },
+		include: { _count: { select: { messages: true } } },
+	})
+
 	const categories = await prismadb.category.findMany()
 
 	return (
 		<article className="h-full space-y-2 p-4">
 			<SearchInput />
 			<Categories data={categories} />
+			<Companions data={data} />
 		</article>
 	)
 }
